f82b3061d08a4ef7e9f7ec9c8b369d8682bacb7b
Francis Musango <francis.musango@imaralli.com>
2026-09-08 15:50:22 +0300

    aqua integration

--- contracts/v2/strategies/ZetaAquaApp.sol
+++ contracts/v2/strategies/ZetaAquaApp.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+import {AquaApp} from "@aqua/src/AquaApp.sol";
+import {IAqua} from "@aqua/src/interfaces/IAqua.sol";
+
 contract ZetaAquaApp is AquaApp {
 
     constructor(IAqua aqua) AquaApp(aqua){
